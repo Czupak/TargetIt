e91e0801d979c0ec47645cce118b482c1e464637
Czupak <lukasz.baranowski@pl.abb.com>
2019-11-16 09:59:21 +0100

    v0.0.5

--- TargetIt.lua
+++ TargetIt.lua
@@ -1,14 +1,22 @@
 local prefix = "|cffffa500Target|cff1784d1It|r: "
-local macro = "/cleartarget\n/target %s\n/stopmacro [dead][noexists]\n/run SetRaidTarget(\"target\",8)"
+local macro = {}
+macro["TargetIt"] = "/cleartarget\n/target %s\n/stopmacro [dead][noexists]\n/tm 8"
+macro["AssistIt"] = "/assist %s\n"
 
 SLASH_TARGETIT1 = '/tit'
 function SlashCmdList.TARGETIT(msg)
-	print(string.format("%sUse /tits to save your current target.\nUse /tits MobName to save custom target.\nUse /titd NAME for pre-defined targets (Currently supports [BRD] for [Shadowforge Flame Keeper])", prefix))
+	print(string.format("%sUse /tits to save your current target.\nUse /tits MobName to save custom target.\nUse /titd NAME for pre-defined targets (Currently supports [BRD] for [Shadowforge Flame Keeper])\nUse /tita [NAME] to save assist target", prefix))
 	if TargetItName == nil 
 	then
 		print(string.format("%sNo target saved.", prefix)) 
 	else
 		print(string.format("%sCurrent target is [%s]", prefix, TargetItName))
+	end
+	if AssistItName == nil 
+	then
+		print(string.format("%sNo assist saved.", prefix)) 
+	else
+		print(string.format("%sCurrent assist is [%s]", prefix, AssistItName))
 	end
 end
 
@@ -31,7 +39,7 @@ function SlashCmdList.TARGETITD(msg)
 	else
 		print(string.format("%s[%s] target set to [%s].", prefix, msg, preds[msg]))
 		TargetItName = preds[msg]
-		setMacro(TargetItName)
+		setMacro(TargetItName, "TargetIt")
 	end
 end
 
@@ -50,18 +58,37 @@ function SlashCmdList.TARGETITS(msg)
 		print(string.format("%sNo target nor custom name. Use /tits to save your current target. Use /tits MobName to save custom target.",prefix))
 	else
 		TargetItName = target
-		setMacro(TargetItName)
+		setMacro(TargetItName, "TargetIt")
 	end
 end
 
-function setMacro(target)
-	local macroId = GetMacroIndexByName("TargetIt")
+SLASH_TARGETITA1 = '/tita'
+function SlashCmdList.TARGETITA(msg)
+    local target
+    if msg == nil or msg == ""
+    then
+        target = UnitName("target");
+    else
+        target = msg
+    end
+   
+    if target == null
+    then
+        print(string.format("%sNo target nor custom name. Use /tita to save your current target for assist macro. Use /tita playerName to save assist target by name.",prefix))
+    else
+        AssistItName = target
+        setMacro(AssistItName, "AssistIt")
+    end
+end
+
+function setMacro(target, macroName)
+	local macroId = GetMacroIndexByName(macroName)
 	if macroId == nil or macroId == 0
 	then
-		macroId = CreateMacro("TargetIt", "INV_MISC_QUESTIONMARK", string.format(macro, target), nil)
-		print(string.format("%sTarget macro for [%s] created.", prefix, target))
+		macroId = CreateMacro(macroName, "INV_MISC_QUESTIONMARK", string.format(macro[macroName], target), nil)
+		print(string.format("%s%s macro for [%s] created.", prefix, macroName, target))
 	else
-		EditMacro("TargetIt", "TargetIt", nil, string.format(macro, target), nil)
-		print(string.format("%sTarget macro for [%s] updated.", prefix, target))
+		EditMacro(macroName, macroName, nil, string.format(macro[macroName], target), nil)
+		print(string.format("%s%s macro for [%s] updated.", prefix, macroName, target))
 	end
 end
